ead6f9176bdff88e741631bcb66aae8d5fe8ea95
Лада Еникеева <ladaenikeeva@yandex.ru>
2023-09-11 21:08:56 +0300

    Удаление лишних классов

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,58 +4,21 @@ from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
 
-from reviews.models import Title, Categories
+from reviews.models import Title, Category
 
 User = get_user_model()
 
 
 class TitleSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
         fields = '__all__'
         model = Title
 
 
-class GroupSerializer(serializers.ModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
 
     class Meta:
-        model = Group
+        model = Category
         fields = '__all__'
 
-
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
-
-    class Meta:
-        fields = '__all__'
-        model = Comment
-        read_only_fields = ('author', 'post')
-
-
-class FollowSerializer(serializers.ModelSerializer):
-    user = SlugRelatedField(
-        slug_field='username',
-        read_only=True,
-        default=serializers.CurrentUserDefault())
-    following = SlugRelatedField(
-        slug_field='username',
-        queryset=User.objects.all())
-
-    class Meta:
-        exclude = ('id',)
-        model = Follow
-        validators = [
-            UniqueTogetherValidator(
-                queryset=Follow.objects.all(),
-                fields=('user', 'following')
-            )
-        ]
-
-    def validate_following(self, value):
-        user = self.context['request'].user
-        if value == user:
-            raise serializers.ValidationError('Нельзя подписаться на себя')
-        return value
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,6 +1,3 @@
-from django.contrib.auth import get_user_model
-from rest_framework.pagination import PageNumberPagination
-
 from rest_framework import viewsets
 
 
@@ -8,16 +5,12 @@ from reviews.models import Title, Category
 from .serializers import (TitleSerializer,
                           CategorySerializer)
 
-User = get_user_model()
-
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
-    pagination_class = PageNumberPagination
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    pagination_class = PageNumberPagination
