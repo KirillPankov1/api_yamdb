f903becd039d4e1631c74edbc22e5b1bc06ddf55
Лада Еникеева <ladaenikeeva@yandex.ru>
2023-09-11 21:59:31 +0300

    fix settings

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,8 +21,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog.apps.ApiConfig',
-    'catalog.apps.ReviewsConfig',
+    'api.apps.ApiConfig',
+    'reviews.apps.ReviewsConfig',
 ]
 
 MIDDLEWARE = [
