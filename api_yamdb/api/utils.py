c128e98dab06a982fdb4e04d01d4ffefea9c8549
Кирилл Панков <kpankov217@gmail.com>
2023-09-26 23:23:29 +0300

    Добавил evn и перенес туда константы

--- api_yamdb/api/utils.py
+++ api_yamdb/api/utils.py
@@ -1,17 +1,20 @@
+import os
+
+from dotenv import load_dotenv
 from django.core.mail import send_mail
-from rest_framework.response import Response
-from rest_framework import status
 from random import choice
 from string import ascii_letters
+
+load_dotenv()
 
 
 def get_confirmation_code():
     return ''.join(choice(ascii_letters) for i in range(12))
 
+
 def send_confirmation_code(email, code):
     subject = 'Your Confirmation Code'
     message = f'Your confirmation code is: {code}'
-    from_email = 'from@example.com'
+    from_email = os.getenv('FROM_EMAIL')
     # This should come from environment variables
     send_mail(subject, message, from_email, [email], fail_silently=False)
-
